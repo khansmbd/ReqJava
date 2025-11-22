e03ae0cc7daf4c0a86b7997fa1244d10e846ce37
Salim Khan <salimmahmudkhan@gmail.com>
2025-11-22 16:49:07 -0500

    2nd commit

---
+++ math/Tanary.java
@@ -0,0 +1,16 @@
+package math;
+
+public class Tanary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = 50;
+		
+		
+		
+	     String PassOrFail = (result >= 70) ? "pass" : "fail";
+	     
+	     System.out.println(PassOrFail);
+	}
+
+}
